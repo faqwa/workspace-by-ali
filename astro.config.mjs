3f017a04bdb342abc9fd5832c4e047299b95a6b4
faqwa <alisaleh0201@gmail.com>
2025-10-13 17:12:59 +1100

    chore: add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // Update YOUR-USERNAME with your GitHub username when deploying
 export default defineConfig({
   site: 'https://writingsbyali-hub.github.io',
   base: '/workspace-by-ali/',
   output: 'server',
-  adapter: undefined
+  adapter: vercel(),
 });
